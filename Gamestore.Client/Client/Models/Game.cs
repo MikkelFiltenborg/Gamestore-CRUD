91547937c31b38d321212f87a7c22e376edb9953
Mikkel F <mikkelinis@gmail.com>
2023-03-15 15:26:46 +0100

    Add project files.

---
+++ Gamestore.Client/Client/Models/Game.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gamestore.Client.Client.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(176)]
+        public required string Name { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public required string Genre { get; set; }
+
+        [Range(0, 150)]
+        public decimal Price { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
---
+++ Gamestore.Client/Client/Models/GameClient.cs
@@ -0,0 +1,64 @@
+﻿namespace Gamestore.Client.Client.Models
+{
+    public static class GameClient
+    {
+        private static readonly List<Game> games = new()
+        {
+            new Game()
+            {
+                Id = 1,
+                Name = "War Thunder",
+                Genre = "Battle",
+                Price = 00.00M,
+                ReleaseDate = new DateTime(2012, 11, 1)
+            },
+            new Game()
+            {
+                Id = 2,
+                Name = "Star Wars Knight of the Old Republic",
+                Genre = "Roleplaying",
+                Price = 49.99M,
+                ReleaseDate = new DateTime(2003, 7, 3)
+            },
+            new Game()
+            {
+                Id = 3,
+                Name = "Call of Duty Modern Warfare 2",
+                Genre = "FPS",
+                Price = 59.99M,
+                ReleaseDate = new DateTime(2009, 11, 10)
+            },
+        };
+
+        public static Game[] GetGames()
+        {
+            return games.ToArray();
+        }
+
+        public static void AddGame(Game game)
+        {
+            game.Id = games.Max(game => game.Id) + 1;
+            games.Add(game);
+        }
+
+        public static Game GetGame(int id)
+        {
+            return games.Find(game => game.Id == id) ?? throw new Exception("Could not find game!");
+        }
+
+        public static void UpdateGame(Game updatedGame)
+        {
+            Game existingGame = GetGame(updatedGame.Id);
+            existingGame.Name = updatedGame.Name;
+            existingGame.Genre = updatedGame.Genre;
+            existingGame.Price = updatedGame.Price;
+            existingGame.ReleaseDate = updatedGame.ReleaseDate;
+        }
+
+        public static void DeleteGame(int id)
+        {
+            Game game = GetGame(id);
+            games.Remove(game);
+        }
+    }
+}
